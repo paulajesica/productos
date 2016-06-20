612be76a0027307be7ac460d5b1d31723e8fac58
paulajesica <pamora055@misena.edu.co>
2016-06-20 09:58:40 +0200

    MODIFICAR

--- PAGINAPRINCIPAL.php
+++ PAGINAPRINCIPAL.php
@@ -36,10 +36,10 @@ body div tt {
   </h1>
 </div>
 <form id="form1" name="form1" method="post" action="">
-  <h2 align="center"><a href="proyecto bases de datos/insertar.html" target="proyecto bases de datos/insertar.html">registrarse</a><br> <input type="image" name="imageField" id="imageField" src="PRODUCTOS DE LAS MAQUINAS DE ASEO/thÑL.jpg" />  
+  <h1 align="center"><a href="proyecto bases de datos/insertar.html" target="proyecto bases de datos/insertar.html">registrarse</a><br> <input type="image" name="imageField" id="imageField" src="PRODUCTOS DE LAS MAQUINAS DE ASEO/thÑL.jpg" />  
     
-  </h2>
-  <p align="center"><tt>CANCELAR COMPRA </tt></p>
+  </h1>
+  <p align="center"><tt><a href="cancelar compras/insertar.html">CANCELAR COMPRA </a></tt></p>
   <h6> <em>TELÉFONOS</em></h6> 
   <h6><em><a href="tel:320 482 0504"data-sb-event="link-call" data-sb-prop-link="tel:320 482 2168">(320) 482 0504</a>-<a href="tel://(313)888 7202" data-sb-event="link-call" data-sb-prop-link="tel:313 888 7202">(313) 888 7202  <img src="http://sitios.claro.com.co/img/upload/whatsapppng_4.jpg" alt="" width="31" height="26" /><img src="PRODUCTOS DE LAS MAQUINAS DE ASEO/aseo.JPG" width="274" height="232" align="absmiddle" /> </h6>
 </form>
